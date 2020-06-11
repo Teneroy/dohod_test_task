f831ea862b76837202fa8ac03636c5b46f0a6188
Teneroy <alexchernishof@gmail.com>
2020-06-11 03:43:18 +0300

    Added drives listing

---
+++ src/org/teneroy/Main.java
@@ -0,0 +1,10 @@
+package org.teneroy;
+
+import org.teneroy.utils.FileSystemUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FileSystemUtils.listLocalDrives();
+    }
+}
---
+++ src/org/teneroy/utils/FileSystemUtils.java
@@ -0,0 +1,4 @@
+package org.teneroy.utils;
+
+public class FileSystemUtils {
+}
