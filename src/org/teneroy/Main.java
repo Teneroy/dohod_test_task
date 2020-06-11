0f6342f0bad7e21f14520b80fd31005458c16d86
Teneroy <alexchernishof@gmail.com>
2020-06-12 02:49:13 +0300

    Added file manipulation method

--- src/org/teneroy/Main.java
+++ src/org/teneroy/Main.java
@@ -2,10 +2,17 @@ package org.teneroy;
 
 import org.teneroy.utils.FileSystemUtils;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Main {
 
     public static void main(String[] args) {
-        FileSystemUtils.listLocalDrives();
-        FileSystemUtils.searchFileByName("sf.txt", "C:\\testSearch");
+
+        System.out.println(Arrays.toString(args));
+
+        //FileSystemUtils.listLocalDrives();
+        //Objects.requireNonNull(FileSystemUtils.searchFileByName("sf", "C:\\testSearch")).forEach(System.out::println);
+        //FileSystemUtils.changeFiles("sf", "C:\\testSearch");
     }
 }
